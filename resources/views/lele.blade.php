b62d630c2cccd5e0da091286628fd893266adae5
alifmusthofa354 <muhammad.alif.musthofa354@gmail.com>
2025-09-30 22:55:48 +0700

    fix ui

--- resources/views/lele.blade.php
+++ resources/views/lele.blade.php
@@ -5,11 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
     <title>Lele Form - Mie Ganas ok deh</title>
-=======
-    <title>Lele Form baru lagi test 2</title>
->>>>>>> parent of 71ec050 (testimg dullu ah)
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
